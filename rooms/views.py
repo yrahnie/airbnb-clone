124d87fecbfd4bdce42690069de23ce55e40bc8b
yr.ahn <yr.ahn@lge.com>
2022-07-07 18:16:34 +0900

    #12.4 Using DetailView CBV

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,7 @@
 from django.http import Http404
 from django.shortcuts import redirect, render
 from django.urls import reverse
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 
 from . import models
 
@@ -16,33 +16,7 @@ class HomeView(ListView):
     context_object_name = "rooms"
 
 
-def room_detail(request, pk):
-    try:
-        room = models.Room.objects.get(pk=pk)
-        return render(request, "rooms/details.html", {"room": room})
-    except models.Room.DoesNotExist:
-        raise Http404()
+class RoomDetail(DetailView):
+    """RoomDetail Definition"""
 
-
-""" def all_rooms(request):
-    page = request.GET.get("page", 1)
-
-    # django paginator 로 대체 가능 #
-    # page = int(page or 1)
-    # page_size = 10
-    # limit = page_size * page
-    # offset = limit - page_size
-    # all_rooms = models.Room.objects.all()[offset:limit]
-    # page_count = ceil(models.Room.objects.count() / page_size)
-
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10, orphans=5)
-    try:
-        rooms = paginator.page(int(page))
-        return render(
-            request,
-            "rooms/home.html",
-            {"page": rooms},
-        )
-    except EmptyPage:
-        return redirect("/") """
+    model = models.Room
