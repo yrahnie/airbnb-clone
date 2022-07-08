f8f036042e9ecce9e74f86357b6a81870ce3bf44
yr.ahn <yr.ahn@lge.com>
2022-07-08 18:10:39 +0900

    #13.10 Finishing Up

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,8 @@
+from django.core.paginator import Paginator
 from django.http import Http404
 from django.shortcuts import redirect, render
 from django.urls import reverse
-from django.views.generic import DetailView, ListView
+from django.views.generic import DetailView, ListView, View
 from django_countries import countries
 
 from . import forms, models
@@ -23,73 +24,88 @@ class RoomDetail(DetailView):
     model = models.Room
 
 
-def search(request):
+class SearchView(View):
+    """SearchView Definition"""
 
-    country = request.GET.get("country")
+    def get(self, request):
 
-    if country:
-        form = forms.SearchForm(
-            request.GET  # request.GET 을 넣어주면 submit 후 입력, 선택했던 값들이 form 에 유지되어 보여짐!
+        country = request.GET.get("country")
+
+        if country:
+            form = forms.SearchForm(
+                request.GET  # request.GET 을 넣어주면 submit 후 입력, 선택했던 값들이 form 에 유지되어 보여짐!
+            )
+            if form.is_valid():
+                city = form.cleaned_data.get("city")
+                country = form.cleaned_data.get("country")
+                room_type = form.cleaned_data.get("room_type")
+                price = form.cleaned_data.get("price")
+                guests = form.cleaned_data.get("guests")
+                bedrooms = form.cleaned_data.get("bedrooms")
+                beds = form.cleaned_data.get("beds")
+                baths = form.cleaned_data.get("baths")
+                instant_book = form.cleaned_data.get("instant_book")
+                superhost = form.cleaned_data.get("superhost")
+                amenities = form.cleaned_data.get("amenities")
+                facilities = form.cleaned_data.get("facilities")
+
+                filter_args = {}
+
+                if city != "Anywhere":
+                    filter_args["city__startswith"] = city  # column-name__lookup-type
+
+                filter_args["country"] = country
+
+                if room_type is not None:
+                    filter_args["room_type"] = room_type
+
+                if price is not None:
+                    filter_args["price__lte"] = price  # less than equals
+
+                if guests is not None:
+                    filter_args["guests__gte"] = guests  # greater than equals
+
+                if bedrooms is not None:
+                    filter_args["bedrooms__gte"] = bedrooms
+
+                if beds is not None:
+                    filter_args["beds__gte"] = beds
+
+                if baths is not None:
+                    filter_args["baths__gte"] = baths
+
+                if instant_book is True:
+                    filter_args["instant_book"] = True
+
+                if superhost is True:
+                    filter_args["host__superhost"] = True
+
+                for amenity in amenities:
+                    filter_args["amenities"] = amenity
+
+                for facility in facilities:
+                    filter_args["facilities"] = facility
+
+                qs = models.Room.objects.filter(**filter_args).order_by("-created")
+
+                paginator = Paginator(qs, 10, orphans=5)
+
+                page = request.GET.get("page", 1)
+
+                rooms = paginator.get_page(page)
+
+                return render(
+                    request,
+                    "rooms/search.html",
+                    {"form": form, "rooms": rooms},
+                )
+        else:
+            form = (
+                forms.SearchForm()  # 검색어 없이 검색했을 때 default 값들로 보여지게. request.GET 이 있으면 required 필드들에 대해 complain 발생함.
+            )
+
+        return render(
+            request,
+            "rooms/search.html",
+            {"form": form},
         )
-        if form.is_valid():
-            city = form.cleaned_data.get("city")
-            country = form.cleaned_data.get("country")
-            room_type = form.cleaned_data.get("room_type")
-            price = form.cleaned_data.get("price")
-            guests = form.cleaned_data.get("guests")
-            bedrooms = form.cleaned_data.get("bedrooms")
-            beds = form.cleaned_data.get("beds")
-            baths = form.cleaned_data.get("baths")
-            instant_book = form.cleaned_data.get("instant_book")
-            superhost = form.cleaned_data.get("superhost")
-            amenities = form.cleaned_data.get("amenities")
-            facilities = form.cleaned_data.get("facilities")
-
-            filter_args = {}
-
-            if city != "Anywhere":
-                filter_args["city__startswith"] = city  # column-name__lookup-type
-
-            filter_args["country"] = country
-
-            if room_type is not None:
-                filter_args["room_type"] = room_type
-
-            if price is not None:
-                filter_args["price__lte"] = price  # less than equals
-
-            if guests is not None:
-                filter_args["guests__gte"] = guests  # greater than equals
-
-            if bedrooms is not None:
-                filter_args["bedrooms__gte"] = bedrooms
-
-            if beds is not None:
-                filter_args["beds__gte"] = beds
-
-            if baths is not None:
-                filter_args["baths__gte"] = baths
-
-            if instant_book is True:
-                filter_args["instant_book"] = True
-
-            if superhost is True:
-                filter_args["host__superhost"] = True
-
-            for amenity in amenities:
-                filter_args["amenities"] = amenity
-
-            for facility in facilities:
-                filter_args["facilities"] = facility
-
-            rooms = models.Room.objects.filter(**filter_args)
-    else:
-        form = (
-            forms.SearchForm()  # 검색어 없이 검색했을 때 default 값들로 보여지게. request.GET 이 있으면 required 필드들에 대해 complain 발생함.
-        )
-
-    return render(
-        request,
-        "rooms/search.html",
-        {"form": form, "rooms": rooms},
-    )
