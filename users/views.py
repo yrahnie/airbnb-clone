278297ea487aeed44aeb56cf0c3d4f9098dc4a18
yr.ahn <yr.ahn@lge.com>
2022-07-11 15:55:55 +0900

    #14.5 Always an Easier Way

--- users/views.py
+++ users/views.py
@@ -1,25 +1,26 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect, render, reverse
+from django.urls import reverse_lazy
 from django.views import View
+from django.views.generic import FormView
 
 from . import forms
 
 
-class LoginView(View):
-    def get(self, request):
-        form = forms.LoginForm(initial={"email": "test@aaa.com"})
-        return render(request, "users/login.html", {"form": form})
+class LoginView(FormView):
+    template_name = "users/login.html"
+    form_class = forms.LoginForm
 
-    def post(self, request):
-        form = forms.LoginForm(request.POST)
-        if form.is_valid():
-            email = form.cleaned_data.get("email")
-            password = form.cleaned_data.get("password")
-            user = authenticate(request, username=email, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect(reverse("core:home"))
-        return render(request, "users/login.html", {"form": form})
+    # reserve - namespace 와 name 을 가지고 실제 url 을 줌 / reserve_lazy - reserve 와 같은데 자동으로 호출하지 x
+    success_url = reverse_lazy("core:home")
+
+    def form_valid(self, form):
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
 
 
 def log_out(request):
