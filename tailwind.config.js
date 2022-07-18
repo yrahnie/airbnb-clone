09ffb6f469c1c8f6b7f2f4c9575fc74327ef7217
yr.ahn <yr.ahn@lge.com>
2022-07-18 14:07:25 +0900

    #20.4 Extending Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./templates/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        "25vh": "25vh",
+        "50vh": "50vh",
+        "75vh": "75vh",
+      },
+      borderRadius: {
+        xl: "1.5rem",
+      },
+    },
   },
   plugins: [],
-}
+};
