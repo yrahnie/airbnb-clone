307a7d915b42bde4c4a76e8ba009c37e24c913a3
yr.ahn <yr.ahn@lge.com>
2022-07-20 11:16:25 +0900

    #21.10 Great User Mixins

--- users/views.py
+++ users/views.py
@@ -18,8 +18,9 @@ class LoginView(mixins.LoggedOutOnlyView, FormView):
     template_name = "users/login.html"
     form_class = forms.LoginForm
 
-    # reserve - namespace 와 name 을 가지고 실제 url 을 줌 / reserve_lazy - reserve 와 같은데 자동으로 호출하지 x
-    success_url = reverse_lazy("core:home")
+    # reserve - namespace 와 name 을 가지고 실제 url 을 줌
+    # reserve_lazy - reserve 와 같은데 자동으로 호출하지 x
+    # success_url = reverse_lazy("core:home")
 
     def form_valid(self, form):
         email = form.cleaned_data.get("email")
@@ -28,6 +29,13 @@ class LoginView(mixins.LoggedOutOnlyView, FormView):
         if user is not None:
             login(self.request, user)
         return super().form_valid(form)
+
+    def get_success_url(self):
+        next_arg = self.request.GET.get("next")
+        if next_arg is not None:
+            return next_arg
+        else:
+            return reverse("core:home")
 
 
 def log_out(request):
@@ -223,7 +231,7 @@ class UserProfileView(DetailView):
         return context
 
 
-class UpdateProfileView(SuccessMessageMixin, UpdateView):
+class UpdateProfileView(mixins.LoggedInOnlyView, SuccessMessageMixin, UpdateView):
     model = models.User
     template_name = "users/update-profile.html"
     fields = (
@@ -265,7 +273,12 @@ class UpdateProfileView(SuccessMessageMixin, UpdateView):
     #     return super().form_valid(form)
 
 
-class UpdatePasswordView(SuccessMessageMixin, PasswordChangeView):
+class UpdatePasswordView(
+    mixins.EmailLoginOnlyView,
+    mixins.LoggedInOnlyView,
+    SuccessMessageMixin,
+    PasswordChangeView,
+):
     template_name = "users/update-password.html"
     success_message = "Password Updated"
 
