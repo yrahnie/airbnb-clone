8e1f81789b52ffd77b4f88dbb3e8129743d0f941
yr.ahn <yr.ahn@lge.com>
2022-07-29 17:23:03 +0900

    #24.1 Room Calendar

--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,4 @@
+from cal import Calendar
 from core import models as core_models
 from django.db import models
 from django.urls import reverse
@@ -126,3 +127,8 @@ class Room(core_models.TimeStampedModel):
         photos = self.photos.all()[1:5]
         print(photos)
         return photos
+
+    def get_calendars(self):
+        this_month = Calendar(2019, 11)
+        next_month = Calendar(2019, 12)
+        return [this_month, next_month]
